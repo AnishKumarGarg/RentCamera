37010709752242d3dba03e270935adc53dd68f6d
aanya garg <aanya0085.becse24@chitkara.edu.in>
2025-11-09 00:18:30 +0530

    added directly to main

---
+++ urbannest360/src/firebase.js
@@ -0,0 +1,23 @@
+// Import Firebase modules
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv6OuZqZDgkgEL_rqnsDvt9G61wwM2Dwg",
+  authDomain: "urbannest360-2e7bb.firebaseapp.com",
+  projectId: "urbannest360-2e7bb",
+  storageBucket: "urbannest360-2e7bb.firebasestorage.app",
+  messagingSenderId: "255062530047",
+  appId: "1:255062530047:web:238ad7b5edfaa4e0caf9ca",
+  measurementId: "G-ZL26CC0YMK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+// ✅ Export Auth instance so you can use it in your App.jsx
+export { auth };
